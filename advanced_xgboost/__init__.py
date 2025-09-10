17f766139230c328ff4fd5ecc8049639f3dfc3a9
krishnareddy78 <kri5hnareddyp3ta@gmail.com>
2025-09-10 11:42:40 -0400

    Create __init__.py

---
+++ advanced_xgboost/__init__.py
@@ -0,0 +1,9 @@
+"""
+Senior XGBoost from Scratch
+
+An advanced implementation of the XGBoost algorithm focusing on the core
+mathematical principles.
+"""
+from .xgboost_model import XGBoost
+
+__version__ = "1.0.0"
